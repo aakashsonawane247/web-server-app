1bc38b8212fd4b8105e8a41860a4e7e8c712fb12
Aakash <aakashsonawane247@gmail.com>
2019-10-09 17:13:38 +0530

    Add High/Low temprature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude,longitude, callback)=>{
         }else if(response.body.error){
             callback('unable to find location, Try another service',undefined) 
         }else{
-                callback(undefined, response.body.daily.data[0].summary +' It is currentely '+ response.body.currently.temperature +' degree out. There is ' +response.body.currently.precipProbability +'% chance of rain.')
+            //console.log(response.body.daily.data[0])
+            callback(undefined, response.body.daily.data[0].summary +' It is currentely '+ response.body.currently.temperature +' degree out. This high today is '+response.body.daily.data[0].temperatureHigh+' with a low of '+response.body.daily.data[0].temperatureLow+'. There is ' +response.body.currently.precipProbability +'% chance of rain.')
         }
      
      })
